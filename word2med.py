06f186e306fa03f77f2850bd0abec91694658f55
Justin Boileau <justin@blindnet.io>
2023-03-09 17:18:12 -0700

    template file for querying model

--- word2med.py
+++ word2med.py
@@ -5,8 +5,8 @@ class Word2Med:
     def get_vector(self, word: str):
         pass
 
-    def get_n_closest(self, word: List[str], n: int):
-        pass
+    def get_n_closest(self, word: List[str], n: int) -> List[str]:
+        return []
 
-    def complete_analogy(self, a: str, b: str, c: str, n: int):
-        pass
+    def complete_analogy(self, a: str, b: str, c: str, n: int) -> List[str]:
+        return []
