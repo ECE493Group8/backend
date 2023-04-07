6e7b85a93844fca5461bfbd1ae4b8133d0f7d13b
Justin Boileau <justin@blindnet.io>
2023-04-07 17:29:26 -0600

    fix tests and remove multiple embeddings test which we don't support

--- test_endpoints.py
+++ test_endpoints.py
@@ -5,32 +5,28 @@ from backend import app
 # TODO: May have to add content-type: json header to responses for response.json to work
 
 def test_vector():
-    response = app.test_client().get('/vector?word=word1')
+    response = app.test_client().get('/vector?word=pain')
+    print(response)
     assert response.status_code == 200
     assert all(key in dict(response.json) for key in ['word', 'vector'])
 
 def test_neighbours_single_word():
-    response = app.test_client().get('/neighbours?words=word1&n=5')
+    response = app.test_client().get('/neighbours?words=pain&n=5')
     assert response.status_code == 200
     assert all(key in dict(response.json) for key in ['words', 'n', 'neighbours'])
 
 def test_neighbours_multiple_words():
-    response = app.test_client().get('/neighbours?words=word1&words=word2&n=5')
+    response = app.test_client().get('/neighbours?words=pain&words=cancer&n=5')
     assert response.status_code == 200
-    assert all(key in dict(response.json) for key in ['a', 'b', 'c', 'n', 'completions'])
+    assert all(key in dict(response.json) for key in ['words', 'n', 'neighbours'])
 
 def test_analogy():
-    response = app.test_client().get('/analogy?a=worda&b=wordb&c=wordc&n=10')
+    response = app.test_client().get('/analogy?a=chemotherapy&b=cancer&c=alzheimer&n=10')
     assert response.status_code == 200
     assert all(key in dict(response.json) for key in ['a', 'b', 'c', 'n', 'completions'])
 
-def test_embeddings_single_word():
-    response = app.test_client().get('/embeddings?words=word1&n=5')
-    assert response.status_code == 200
-    assert all(key in dict(response.json) for key in ['words', 'n', 'words_list', 'embeddings_list'])
-
 def test_embeddings_multiple_words():
-    response = app.test_client().get('/embeddings?words=word1&words=word2&n=5')
+    response = app.test_client().get('/embeddings?words=pain&words=back&words=boy&n=10')
     assert response.status_code == 200
     assert all(key in dict(response.json) for key in ['words', 'n', 'words_list', 'embeddings_list'])
 
